42d193b7ad16a44a9bae451acfb3e32cb7ead733
Gabriel Torquato <86808161+gabrielctor@users.noreply.github.com>
2024-03-12 00:06:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
 function Home() {
-    return <h1>Amo você, gosto tanto de você, da pontinha do pé, até a ponta do nariz! 🖤</h1>
+    return (
+        <h1>
+            Amo você, gosto tanto de você, da pontinha do pé, até a ponta do
+            nariz! 🖤
+        </h1>
+    );
 }
+
+function teste() {
+    console.log("teste");
+}
+
+function teste2() {
+    console.log("teste2");
+}
+
 export default Home;
